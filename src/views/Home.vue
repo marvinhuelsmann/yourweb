45cce20459ec2b4be2923db54e584b599a385c09
Marvin Hülsmann <mrntv64@gmail.com>
2021-04-20 17:46:13 +0200

    fixing some text bugs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
               Entwicklungsstart am 1.5.2021
             </span>
             <span class="hidden md:inline">
-              Große Neuigkeiten! Wir entwickeln an der Max Seite.
+              Große Neuigkeiten! Wir sind am entwickeln!
             </span>
           </p>
         </div>
@@ -23,7 +23,7 @@
         </div>
         <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
           <button type="button" formtarget="_top"
-                  onclick="window.location.href='https://twitter.com/intent/tweet?text=YourWebsite is now development on the Max Site! #YourWebsite'"
+                  onclick="window.location.href='https://twitter.com/intent/tweet?text=yourweb.monster is now starting to developed! #YourWebsite'"
                   class="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
             <span class="sr-only">Dismiss</span>
             <ShareIcon class="h-6 w-6 text-white" aria-hidden="true"/>
