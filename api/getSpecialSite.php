ba139167bd322d8a5a4167658a77ebdab11a7125
Marvin Hülsmann <mrntv64@gmail.com>
2021-05-03 22:26:26 +0200

    fixing Community View

--- api/getSpecialSite.php
+++ api/getSpecialSite.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
-getUser(isset($_GET['oldUser']) ?? $_GET['oldUser']);
+getUser(!isset($_GET['oldUser']) ? '' : $_GET['oldUser']);
 
 function getUser($userID)
 {
