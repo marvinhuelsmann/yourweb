0d9f66c39b2c0678b395a28ef3f92fd3c5c9f999
Marvin Hülsmann <mrntv64@gmail.com>
2021-05-03 23:09:51 +0200

    fixing small end Community.vue bug

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -71,21 +71,26 @@ export default {
     reload() {
       this.isLoaded = false
       if (this.user.id === null || this.user.id === "null") {
-        fetch('https://yourweb.monster/api/v1/getCommunity')
-      } else {
-        fetch('https://yourweb.monster/api/v1/getCommunity?oldUser=' + this.user.id)
-      }
-    },
-    fetch(url) {
-      fetch(url).then(result => {
-        result.json().then(result => {
-          this.user = result
-        }).finally(() => {
-          this.isLoaded = true
+        fetch('https://yourweb.monster/api/v1/getCommunity').then(result => {
+          result.json().then(result => {
+            this.user = result
+          }).finally(() => {
+            this.isLoaded = true
+          })
+        }).catch(error => {
+          console.error(error)
         })
-      }).catch(error => {
-        console.error(error)
-      })
+      } else {
+        fetch('https://yourweb.monster/api/v1/getCommunity?oldUser=' + this.user.id).then(result => {
+          result.json().then(result => {
+            this.user = result
+          }).finally(() => {
+            this.isLoaded = true
+          })
+        }).catch(error => {
+          console.error(error)
+        })
+      }
     }
   }
 }
