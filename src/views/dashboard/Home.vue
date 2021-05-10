be5e5cdc21d6c698c7562c47d5f4f6b436e0e98f
Marvin Hülsmann <mrntv64@gmail.com>
2021-05-10 18:34:08 +0200

    fixing small dashboard bug

--- src/views/dashboard/Home.vue
+++ src/views/dashboard/Home.vue
@@ -43,7 +43,7 @@
             <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
               <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                 <div class="flex items-center justify-between w-full md:w-auto">
-                  <a href="#">
+                  <a href="/">
                     <span class="sr-only">Workflow</span>
                     <img class="h-8 w-auto sm:h-10" src="../../assets/images/yourweb.png" alt=""/>
                   </a>
