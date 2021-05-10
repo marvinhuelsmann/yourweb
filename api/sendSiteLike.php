1f76d0190068e40f932969fc77aa437e23c9cb3f
Marvin Hülsmann <mrntv64@gmail.com>
2021-05-10 10:06:00 +0200

    fixing some like bugs

--- api/sendSiteLike.php
+++ api/sendSiteLike.php
@@ -1,5 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 require 'util/config.php';
 
