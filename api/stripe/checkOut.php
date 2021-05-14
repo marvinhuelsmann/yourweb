1cded6688e81264c33e469469d5dc91575046f08
Marvin Hülsmann <mrntv64@gmail.com>
2021-05-14 22:14:58 +0200

    adding stripe managment

---
+++ api/stripe/checkOut.php
@@ -0,0 +1,33 @@
+<?php
+
+use Stripe\Checkout\Session;
+use Stripe\Exception\ApiErrorException;
+use Stripe\Stripe;
+
+Stripe::setApiKey('sk_test_51IqyqrH6quEIoqbt3k52nYAguXLprYxVHDLZ3cXsjhMi7FMJgEXMy0o7My9SA4tauxgUNbd1ucbWkBTa9B7dslp100b5BVqVtY');
+header('Content-Type: application/json');
+
+$DOMAIN = 'http://localhost:8080';
+
+try {
+    $checkout_session = Session::create([
+        'payment_method_types' => ['card'],
+        'line_items' => [[
+            'price_data' => [
+                'currency' => 'usd',
+                'unit_amount' => 1000,
+                'product_data' => [
+                    'name' => 'Stubborn Attachments',
+                    'images' => ["https://i.imgur.com/EHyR2nP.png"],
+                ],
+            ],
+            'quantity' => 1,
+        ]],
+        'mode' => 'payment',
+        'success_url' => $DOMAIN . '/success.html',
+        'cancel_url' => $DOMAIN . '/',
+    ]);
+} catch (ApiErrorException $e) {
+}
+
+echo json_encode(['id' => $checkout_session->id]);
