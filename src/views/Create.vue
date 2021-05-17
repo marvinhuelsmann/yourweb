f637c3329d0582cacad19f734c4e49363d27a1cf
Marvin Hülsmann <mac@marvhuelsmann.de>
2021-05-17 22:39:23 +0200

    adding img target function

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -3,8 +3,10 @@
        class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
       <div>
+        <a href="https://yourweb.monster">
         <img class="mx-auto h-12 w-auto" src="../assets/images/yourweb.png"
              alt="Workflow"/>
+        </a>
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
           mini Monster Seite erstellen
         </h2>
