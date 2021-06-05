fd5810451630b7540d9094bcb4782ba7de7426da
Marvin Hülsmann <mrntv64@gmail.com>
2021-06-05 23:43:55 +0200

    adding discord url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,9 +16,9 @@
           </p>
         </div>
         <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-          <a href="https://twitter.com/MonsterYourWeb" target="_blank"
+          <a href="https://discord.gg/vHdefPjSxQ" target="_blank"
              class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50">
-            Erfahre mehr
+            YourWeb auf Discord
           </a>
         </div>
         <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
