723f337855506493ce8e98c7a6ff32c689c4a526
Marvin Hülsmann <mac@marvhuelsmann.de>
2021-06-05 18:24:32 +0200

    fixing dashboard animation responsive bug (closes #16)

--- src/views/dashboard/Site.vue
+++ src/views/dashboard/Site.vue
@@ -328,7 +328,7 @@
                 <div v-else>
                   <link rel="stylesheet" href="https://pagecdn.io/lib/font-awesome/5.10.0-11/css/all.min.css"
                         integrity="sha256-p9TTWD+813MlLaxMXMbTA7wN/ArzGyW/L7c5+KkjOkM=" crossorigin="anonymous">
-                  <div class="w-full h-full fixed block top-0 left-10 opacity-75 z-50">
+                  <div class="w-full h-full fixed block top-0 lg:top-10 lg:left-10 right-10 opacity-75 z-50">
           <span class="text-purple-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0" style="top: 30%">
             <i class="fas fa-circle-notch fa-spin fa-5x"></i>
           </span>
